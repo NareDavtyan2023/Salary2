e5ecebf15f1236e924015ec73b771beeca66ed92
Admin <Admin@DESKTOP-A06UMIO>
2023-06-12 23:00:38 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Salary2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+    
+        {
+            Console.WriteLine("Enter a number:");
+            double Salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the percentage:");
+            double percent = double.Parse(Console.ReadLine());
+
+            double Sum = CalculatePercentage(Salary, percent);
+
+            Console.WriteLine("Result: " + Sum);
+        }
+
+        static double CalculatePercentage(double Salary, double percent)
+        {
+            return Salary= Salary-(Salary * percent) / 100.0;
+        }
+    }
+}
